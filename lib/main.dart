fa5384359de88ca30026bc663ce1fa9c5a4f7385
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-02 19:41:51 +0500

    Setup firebase and supabase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:ecommerce_shopping_store/View/Screens/HomeScreen.dart';
+import 'package:ecommerce_shopping_store/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

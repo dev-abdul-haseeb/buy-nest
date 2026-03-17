c39e6064dd8d7105260a022900f3de670844acb2
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-17 14:50:25 +0500

    Add obscure text bloc

---
+++ lib/ViewModel/Bloc/obscure_text/obscure_text_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'obscure_text_event.dart';
+part 'obscure_text_state.dart';
+
+class ObscureTextBloc extends Bloc<ObscureTextEvent, ObscureTextState> {
+
+  ObscureTextBloc() : super(ObscureTextState()) {
+    on<ToggleObscure>(_toggleObscure);
+    on<ClearObscureFiled>(_clearObscureField);
+  }
+
+  void _toggleObscure (ToggleObscure event, Emitter<ObscureTextState> emit) {
+    emit(state.copyWith(newBool: !state.obscureText));
+  }
+  void _clearObscureField (ClearObscureFiled event, Emitter<ObscureTextState> emit) {
+    emit(state.copyWith(newBool: true));
+  }
+
+
+}

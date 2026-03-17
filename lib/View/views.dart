56c81d867a316bc27251e9cbc95587bc6f5b0b12
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-17 14:52:51 +0500

    Add views.dart

---
+++ lib/View/views.dart
@@ -0,0 +1,15 @@
+
+//Screens
+export 'package:ecommerce_shopping_store/View/splash_screen/splash_screen.dart';
+
+export 'package:ecommerce_shopping_store/View/auth_navigator/auth_navigator.dart';
+
+export 'package:ecommerce_shopping_store/View/authentication/login_screen/login_screen.dart';
+export 'package:ecommerce_shopping_store/View/authentication/signup_screen/signup_screen.dart';
+export 'package:ecommerce_shopping_store/View/authentication/reset_password_screen/reset_password_screen.dart';
+
+
+export 'package:ecommerce_shopping_store/View/home_screen/home_screen.dart';
+
+//Configs
+export '../../../config/enums/enums.dart';

f6b322af23519ec0d77792f87dce3fb9ae1d4f93
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-19 13:48:09 +0500

    Add seller entry point

---
+++ lib/View/Seller/main_screen/seller_main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SellerMainScreen extends StatefulWidget {
+  const SellerMainScreen({super.key});
+
+  @override
+  State<SellerMainScreen> createState() => _SellerMainScreenState();
+}
+
+class _SellerMainScreenState extends State<SellerMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Seller'),
+      ),
+    );
+  }
+}

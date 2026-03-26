872b367c36513c6232500382eb7fc8834b97dd10
Abdul Haseeb <abdulhaseeb.39393@gmail.com>
2026-03-26 17:26:38 +0500

    Changed the color of error

--- lib/config/color/colors.dart
+++ lib/config/color/colors.dart
@@ -4,20 +4,19 @@ enum appColors{
   accentColor,
   appBGColor,
   cardColor,
-  expenseColor,
+  errorColor,
   incomeColor,
   textPrimaryColor,
   textSecondaryColor,
 }
 
 class AppColors {
-
   static const Map<appColors, Color> lightTheme = {
     appColors.primaryColor       : Color(0xFF5D7ADD),          //Navy: For appbar, icons, main UI
     appColors.accentColor        : Color(0xFFD4AF37),           //Gold: Buttons, highlights, active tabs
     appColors.appBGColor         : Color(0xFFE1E4E8),            //Light grey: For background
     appColors.cardColor          : Color(0xFFFFFFFF),             //White: For cards and containers
-    appColors.expenseColor       : Color(0xFFEF4444),          //Soft red: For expense
+    appColors.errorColor         : Colors.red,          //Soft red: For expense
     appColors.incomeColor        : Color(0xFF22C55E),           //Fresh green: For income
     appColors.textPrimaryColor   : Color(0xFF0F172A),
     appColors.textSecondaryColor : Color(0xFF64748B),
@@ -28,7 +27,7 @@ class AppColors {
     appColors.accentColor        : Color(0xFFD4AF37),
     appColors.appBGColor         : Color(0xFF0F172A),
     appColors.cardColor          : Color(0xFF1E293B),
-    appColors.expenseColor       : Color(0xFFFF6B6B),
+    appColors.errorColor         : Colors.red,
     appColors.incomeColor        : Color(0xFF4ADE80),
     appColors.textPrimaryColor   : Color(0xFFF1F5F9),
     appColors.textSecondaryColor : Color(0xFF94A3B8),
